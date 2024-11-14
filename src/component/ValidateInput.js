5c1e55ec36a15a6d18318b025c5ab066a19af5ad
Alan Zapadka <alanzapadka@yahoo.com>
2024-11-14 20:55:34 +0000

    valid_code

--- src/component/ValidateInput.js
+++ src/component/ValidateInput.js
@@ -1,2 +1,33 @@
-import React, {useState, useEffect} from React
+import React, { useState, useEffect, useReducer } from "react";
+import './Validate.css';
+const ValidatedInput = ({
+}) => {
+const [Valid, setValid] = useState(true)
 
+const[userError, setUserError] = useState(null)
+
+
+return(
+    <form >
+        <div>
+            <label>Username:</label>
+            <input type="text" name="username" placeholder="Username" autoComplete="off"  />
+     
+        </div>
+        <div>
+            <label>Email:</label>
+            <input type="text" name="email" placeholder="Email" autoComplete="off"  >
+    
+            </input>
+            </div>
+            <div>
+            <label>Password:</label>
+            <input type="text" name="password" placeholder="password" autoComplete="off" >
+ 
+            </input>
+            </div>
+            <button type="submit">Submit</button>
+    </form>
+)}
+
+export default ValidatedInput
